0d2339a5a805407ab6f10e334f8cf59e91ff7e18
amillman <amillman@ctemc.org>
2013-12-24 13:29:22 -0500

    added animation for blocks to slide away and reveal content behind

--- js/new-index.js
+++ js/new-index.js
@@ -13,4 +13,40 @@ $(document).ready(function() {
 
     });
 
+    $(".section-block").click(function(){
+
+        _expandBlocks("yes");
+
+    });
+
 });
+
+
+_expandBlocks = function(id) {
+
+    $("." + id + "-content").fadeIn(200);
+
+    $("#web-block").animate({
+        left: "-51%",
+        top:"-51%"
+    }, 400);
+
+    $("#android-block").animate({
+        right: "-51%",
+        top:"-51%"
+    }, 400);
+
+    $("#research-block").animate({
+        left: "-51%",
+        bottom:"-51%"
+    }, 400);
+
+    $("#resume-block").animate({
+        right: "-51%",
+        bottom:"-51%"
+    }, 400);
+
+    $(".menu-icon-line").css("background-color","#555555");
+    $("#andrew-info").css("color","#555555");
+
+}
