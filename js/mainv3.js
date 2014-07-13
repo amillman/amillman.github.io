2b060131c33d03bdbe19dfdd84841167e11a22d4
amillman <andrewmillman35@gmail.com>
2014-07-13 01:14:57 -0400

    Add side menu with a blurred background

--- js/mainv3.js
+++ js/mainv3.js
@@ -1,9 +1,28 @@
 $(document).ready( function() {
 
+  adjustBgSize();
+
   $(".menu-button-wrapper").click(function() {
-    var button = $(".menu-button");
-    if ( !button.hasClass("open") ) { button.addClass("open").removeClass("normal") }
-    else { button.addClass("normal").removeClass("open") }
+    var menuAndButton = $(".side-menu-wrapper, .menu-button");
+    if ( !menuAndButton.hasClass("open") ) { menuAndButton.addClass("open").removeClass("normal") }
+    else { menuAndButton.addClass("normal").removeClass("open") }
   })
 
+  $(window).resize(function() {
+    adjustBgSize();
+  });
+
 });
+
+function adjustBgSize() {
+
+  var windowWidth = $(window).width();
+  var windowHeight = $(window).height();
+
+  $(".bg-blur").width(windowWidth); //adjust size of the blur backgrounds
+  if ( windowWidth < 1.60 * windowHeight ) {
+    $("html, .bg-blur").css("background-size", "auto 100%");
+  } else {
+    $("html, .bg-blur").css("background-size", "100% auto");
+  }
+}
