6757a8a6dd1a6f1d30d75f046837dfda42550cf9
Andrew Millman <andrewmillman35@gmail.com>
2017-01-07 19:54:59 -0500

    Add content

--- js/content-info.js
+++ js/content-info.js
@@ -3,8 +3,8 @@ var contentInfo = {
         {
             'title': 'Pinterest',
             'image': 'pinterest.png',
-            'slogan': 'The World\'s Catalog of Ideas',
-            'description': 'Pintersdfsdfs df sdf sdf sd fs df sf ds fd e',
+            'slogan': 'The world\'s catalog of ideas',
+            'description': 'I interned at Pinterest in Summer 2016 as a Software Engineer. As an iOS Engineer on the Growth Engineering team, I implemened a new UI Component, called the Action Prompt, which is a window that pops up on the user\'s screen to educate them about core features of the app, such as repinning pins to boards, or creating new boards. The action prompt also prompts users to take actions related to the education and allows them to do so directly. I also wrote backend logic for iOS features I worked on to handle the unique requirements of different action prompts, such as fetching the user\'s browsing history or engagement level in Pinterest. Another feature I worked on was Screenshot Repins, which automatically repins pins that a user screenshots to a special board called "Screenshots". For all of these features, I ran A/B experiments to test their effectiveness on core metrics.',
             'link': 'https://www.pinterest.com/',
             'class': 'pinterest',
             'round': true
@@ -12,16 +12,16 @@ var contentInfo = {
         {
             'title': 'Vurb',
             'image': 'vurb.png',
-            'slogan': 'Find and Save Things to Do',
-            'description': 'Pintersdfsdfs df sdf sdf sd fs df sf ds fd e',
+            'slogan': 'Find and save things to do',
+            'description': 'As an iOS Intern for Vurb in Summer 2015, I worked on the app\'s Group Chat feature, an extension of the existing one-to-one messaging feature. I was mainly responsible for the main chats list page, the edit group chat page, and the create group chat page. In the main list page, I took the existing UI for the one-to-one page and updated it to account for groups. In the edit group chat page, I implemented features such as changing the group name and adding/deleting users from the chat. In the create group chat page, I extended existing functionality for selecting Vurb friends or contacts and allowing the user to select multiple people, choose a group name, and begin chatting. All of this also involved allowing the app to respond to events in real time as well. In addition to the features, I wrote Automation Tests with KIF-Kiwi and some Xcode Unit Tests to ensure functionality. ',
             'link': 'https://www.vurb.com/',
             'class': 'vurb'
         },
         {
             'title': 'Artsicle',
             'image': 'artsicle.png',
-            'slogan': 'Find and Save Things to Do',
-            'description': 'Pintersdfsdfs df sdf sdf sd fs df sf ds fd e',
+            'slogan': 'Find amazing art, direct from the artist\'s studio',
+            'description': 'I interned at Artsicle in New York City in Spring 2014. I worked in various parts of the app as a Full-Stack Engineer. I implemented the Help Center from the ground up, which is a resource hub for users, specifically artists to refer to if they have trouble using the app. This involved building the Ruby on Rails resources, bringing the design to life, and plugging it into the existing admin portal so admins from Artsicle could easily create and edit the content. The second major feature I worked on was a design makeover for the messaging feature, which involved creating a split-view experience for users to easily navigate and view their messages.',
             'link': 'https://www.artsicle.com/',
             'class': 'artsicle'
         },
@@ -29,7 +29,7 @@ var contentInfo = {
             'title': 'PureHistory',
             'image': 'purehistory.png',
             'slogan': 'Bringing history to people of all ages.',
-            'description': 'At PureHistory (P) you will be connecting with schools K-12, colleges, universities, libraries, museums, churches, corporations, organizations, building your family tree, building your profile page, media, movies, historical documentaries, music, sports, historical events, over 300 university writers, purehistory book club, destination of historical information around the country and around the world. PureHistory is a database for everybody, from children to elders, and provides an array of engaging articles and topics.',
+            'description': 'In Fall 2013, I worked with PureHistory to bring out an Android app that gave users anAt PureHistory (P) you will be connecting with schools K-12, colleges, universities, libraries, museums, churches, corporations, organizations, building your family tree, building your profile page, media, movies, historical documentaries, music, sports, historical events, over 300 university writers, purehistory book club, destination of historical information around the country and around the world. PureHistory is a database for everybody, from children to elders, and provides an array of engaging articles and topics.',
             'link': 'https://play.google.com/store/apps/details?id=com.purehistory.purehistory&hl=en',
             'class': 'purehistory'
         }
@@ -38,16 +38,16 @@ var contentInfo = {
         {
             'title': 'SAGE',
             'image': 'sage.png',
-            'slogan': 'For the nonprofit SAGE Mentorship.',
-            'description': 'Watershed is an app made by Blueprint at UC Berkeley for the nonprofit organization Project Watershed. By planting various plants around and maintaining them, people can protect local watersheds and keep them clean, ultimately preserving our ecosystem. In order to keep fluid maintenance of the plants planted around the Bay Area, employees and community members need some efficient way to communicate what various plants need, who will maintain them, and how they will be maintained. The Watershed App, for both iOS and Android (personally worked on the iOS app), allows users to do all that. It allows them to keep track of the status of plants planted around the Bay Area and lets managers easily distribute tasks to users. The app is still currently under development and will be put on the App Store / Play Store.',
-            'link': 'https://github.com/calblueprint/sage-mobile',
+            'slogan': 'For the nonprofit SAGE Mentorship',
+            'description': 'SAGE Mentorship is a nonprofit at UC Berkeley that connects student mentors to youth in local communities. Mentors would be paired with elementary school students, and engage in activities such as playing sports, eating, or simply giving the younger ones advice. The SAGE Mentorship app (personally worked on the iOS app) is a solution to the problem the organization was having of recording and maintaining the hours that the mentors worked. By moving the pen and paper system to a digital system, the app allows mentors to easily check in their mentoring sessions as the app verifies their location. It also provides and easy and clean way for mentors to track their progress and hours throughout the school semester. Admins can also track mentors\' progress and make sure they are consistent with their sessions. The app is currently out on the Apple Store, as well as the Google Play Store.',
+            'link': 'https://itunes.apple.com/us/app/sage-mentorship/id1082583314?mt=8',
             'class': 'sage'
         },
         {
             'title': 'Watershed',
             'image': 'watershed.png',
-            'slogan': 'For the nonprofit Project Watershed.',
-            'description': 'Watershed is an app made by Blueprint at UC Berkeley for the nonprofit organization Project Watershed. By planting various plants around and maintaining them, people can protect local watersheds and keep them clean, ultimately preserving our ecosystem. In order to keep fluid maintenance of the plants planted around the Bay Area, employees and community members need some efficient way to communicate what various plants need, who will maintain them, and how they will be maintained. The Watershed App, for both iOS and Android (personally worked on the iOS app), allows users to do all that. It allows them to keep track of the status of plants planted around the Bay Area and lets managers easily distribute tasks to users. The app is still currently under development and will be put on the App Store / Play Store.',
+            'slogan': 'For the nonprofit Project Watershed',
+            'description': 'Watershed is an app made by Blueprint at UC Berkeley for the nonprofit organization Project Watershed. By planting various plants around and maintaining them, people can protect local watersheds and keep them clean, ultimately preserving our ecosystem. In order to keep fluid maintenance of the plants planted around the Bay Area, employees and community members need some efficient way to communicate what various plants need, who will maintain them, and how they will be maintained. The Watershed App, for both iOS and Android (personally worked on the iOS app), allows users to do all that. It allows them to keep track of the status of plants planted around the Bay Area and lets managers easily distribute tasks to users.',
             'link': 'https://github.com/calblueprint/watershed',
             'class': 'watershed'
         },
@@ -63,7 +63,7 @@ var contentInfo = {
         {
             'title': 'CityScenes',
             'image': 'cityscenes.png',
-            'slogan': 'Explore the city by bike.',
+            'slogan': 'Explore the city by bike',
             'description': 'CityScenes allows you to travel around New York City with CitiBike\'s bike sharing program and experience a scenic route on the way. It uses the Google Maps API and open NYC data of tourist locations that gives you the optimal route from point A to point B using a CitiBike and passing through the desired number of locations. This app was a hack made during the Young Rewired State (YRS) Hackathon in NYC. It won the \'Best in Show\' award and gave our team the opportunity to travel to the UK to participate in the YRS UK Hackathon. Skills used include HTML, CSS, JavaScript, Bootstrap, Google Maps API, and Yahoo! Weather API.',
             //'link': 'http://cityscenes.tk',
             'class': 'cityscenes',
